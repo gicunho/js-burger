887b6d5dfe5db04ee9aec638c9dbe2129dbb7bdb
gicunho <gicunho@hotmail.it>
2021-05-05 00:05:18 +0200

    Aggiunto graffica e personalizzazione css

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,29 +3,33 @@
 //Creare lista di ingrediente e assegnare prezzo a ciascuno
 var ingredienti = [
     ['cheese', '1'],
-    ['egg', '1.5'],
-    ['mustard', '0.5'],
     ['tomato', '1'],
+    ['egg', '1.5'],
     ['lettuce', '2'],
+    ['mustard', '0.5'],
     ['ketchup', '0.5'],
 ]
 
+var coupons = ['boolean20', 'gicunho20']
 function generazioneIngredienti (list, el){
     for (i= 0; i < list.length; i++) {
-        el.insertAdjacentHTML('afterbegin',
+        el.insertAdjacentHTML('beforeend',
         `
-        <div class="form-group">
-            <img width=30 src="./assets/img/${list[i][0]}.svg"> </img>
-        
-            <label for="${list[i][0]}">${list[i][0]}</label>
-            <input type="checkbox" name="${list[i][0]}" id="${list[i][0]}" data-price="${list[i][1]}">
+        <div class="form-group my-2">
+            <img width=50 src="./assets/img/${list[i][0]}.svg"> </img>
+            <span class="text-capitalize font-weight-bold mx-2">${list[i][0]}</span>
+            <label class="checkbox text-pink">add
+            <input type="checkbox" name="${list[i][0]}" id="${list[i][0]}" data-price="${list[i][1]}" class="mx-2">
+            <span class="checkmark"></span>
+            </label>
         </div>
         `
         )
     }
 }
 
-generazioneIngredienti (ingredienti, document.getElementById('ingredients'))
+generazioneIngredienti (ingredienti, document.getElementById('ingredients'));
+
 //Incremento prezzo panino quando un ingrediente viene checkato
 document.getElementById('price_generator').addEventListener('click', function (){
     //Definire prezzo base panino
@@ -38,14 +42,32 @@ document.getElementById('price_generator').addEventListener('click', function ()
     for (i= 0; i < checks.length; i++) {
         var element = checks[i];
         if (element.checked){
-            sommaIngredienti += Number(element.getAttribute('data-price'))
+            sommaIngredienti += Number(element.getAttribute('data-price'));
         }
     }
-    console.log(sommaIngredienti);
 
     var prezzoTotale = prezzoPanino + sommaIngredienti;
-    console.log(prezzoTotale);
+
+    
+    for (j= 0; j < checks.length; j++) {
+        if (document.getElementById('coupon').value == coupons[j]) {
+            prezzoTotale = prezzoTotale - (prezzoTotale * 20)/100;
+        }
+    }
+    document.querySelector('#price').innerHTML = '$ ' + prezzoTotale;
 })
 //Definire codici sconto e calcolo prezzo finale scontato
 //Funzione per pulsante di generazione prezzo
 
+/* 
+        `
+        <div class="form-group my-2">
+            <img width=50 src="./assets/img/${list[i][0]}.svg"> </img>
+        
+            <label for="${list[i][0]}" class="checkbox text-capitalize font-weight-bold mx-2">${list[i][0]}</label>
+            <input type="checkbox" name="${list[i][0]}" id="${list[i][0]}" data-price="${list[i][1]}" class="mx-2">
+            
+            <span class="text-pink">add</span>
+        </div>
+        `
+         */
