338dbd16c12d2dd2fdc80ae1e7fb939f13c5b72b
gicunho <gicunho@hotmail.it>
2021-05-05 00:20:56 +0200

    Aggiunto commenti

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,7 +10,15 @@ var ingredienti = [
     ['ketchup', '0.5'],
 ]
 
+//Definire codici sconto
 var coupons = ['boolean20', 'gicunho20']
+
+/**
+ * ### Generatore contenuto HTML per ingredienti
+ * 
+ * @param {object} list - lista array con ingredienti e prezzi
+ * @param {object} el - collegamento a HTML per inserire il contenuto
+ */
 function generazioneIngredienti (list, el){
     for (i= 0; i < list.length; i++) {
         el.insertAdjacentHTML('beforeend',
@@ -28,15 +36,17 @@ function generazioneIngredienti (list, el){
     }
 }
 
+/* Richiamo funzione per generare gli ingredienti */
 generazioneIngredienti (ingredienti, document.getElementById('ingredients'));
 
-//Incremento prezzo panino quando un ingrediente viene checkato
+// Pulsante "calculate" per generare il prezzo con relativi extra con funzione anonima
 document.getElementById('price_generator').addEventListener('click', function (){
     //Definire prezzo base panino
     var prezzoPanino = 10;
 
     var checks = document.querySelectorAll("input[type='checkbox']");
-
+   
+    //Incremento prezzo panino quando un ingrediente viene checkato
     var sommaIngredienti = null;
 
     for (i= 0; i < checks.length; i++) {
@@ -48,26 +58,14 @@ document.getElementById('price_generator').addEventListener('click', function ()
 
     var prezzoTotale = prezzoPanino + sommaIngredienti;
 
-    
+    //verifica codice sconto
     for (j= 0; j < checks.length; j++) {
         if (document.getElementById('coupon').value == coupons[j]) {
+            
+            //e calcolo prezzo finale scontato
             prezzoTotale = prezzoTotale - (prezzoTotale * 20)/100;
         }
     }
     document.querySelector('#price').innerHTML = '$ ' + prezzoTotale;
 })
-//Definire codici sconto e calcolo prezzo finale scontato
-//Funzione per pulsante di generazione prezzo
 
-/* 
-        `
-        <div class="form-group my-2">
-            <img width=50 src="./assets/img/${list[i][0]}.svg"> </img>
-        
-            <label for="${list[i][0]}" class="checkbox text-capitalize font-weight-bold mx-2">${list[i][0]}</label>
-            <input type="checkbox" name="${list[i][0]}" id="${list[i][0]}" data-price="${list[i][1]}" class="mx-2">
-            
-            <span class="text-pink">add</span>
-        </div>
-        `
-         */
